ba78b5ffd134c1ed70b4987a127b0379b65f195e
Abi M.Sangarab <abi@singiro.com>
2016-11-26 16:05:42 +0100

    Sample collecting

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def authtenticate():
     print(filename)
     result = dict(result=recognise.recognise_voice(filename))
     print(result)
-    recognise.collect('adela', filename)
+    #recognise.collect('adela', filename)
     return result
 
 @post('/speech/text')
